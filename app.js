475163f57d26bb086b5e0522eccf76bfd0ee51c6
CaolanCode <cmacmahon3@gmail.com>
2022-12-23 13:19:52 +0000

    display books in container

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let myLibrary = [];
+let library = [];
 
 function Book(title, author, pages, read){
   this.title = title;
@@ -8,21 +8,39 @@ function Book(title, author, pages, read){
 }
 
 function addBookToLibrary(book){
-  myLibrary.push(book);
+  library.push(book);
 }
 
 const book1 = new Book("Atomic Habits", "James Clear", 320, "yes");
 addBookToLibrary(book1);
-console.log(myLibrary[0]);
+console.log(library[0]);
 const book2 = new Book("The Thursday Murder Club", "Richard Osman", 400, "no");
 addBookToLibrary(book2);
-console.log(myLibrary[1]);
+console.log(library[1]);
 const book3 = new Book("The Bullet that missed", "Richard Osman", 432, "no");
 addBookToLibrary(book3);
-console.log(myLibrary[2]);
+console.log(library[2]);
 
-const libraryContainer = document.querySelector(".library-container");
-const bookContainer = document.createElement("div");
-bookContainer.classList.add('bookContainer');
-bookContainer.textContent = `${book1.title}, ${book1.author}, ${book1.pages}, ${book1.read}`;
-libraryContainer.appendChild(bookContainer);
+
+for(let book in library){
+  const libraryContainer = document.getElementById('libraryContainer');
+  const bookContainer = document.createElement('div');
+  bookContainer.classList.add('bookContainer')
+  const title = document.createElement('p');
+  const author = document.createElement('p');
+  const pages = document.createElement('p');
+  const read = document.createElement('p');
+  title.textContent = library[book].title;
+  author.textContent = library[book].author;
+  pages.textContent = library[book].pages;
+  read.textContent = library[book].read;
+  bookContainer.appendChild(title);
+  bookContainer.appendChild(author);
+  bookContainer.appendChild(pages);
+  bookContainer.appendChild(read);
+  libraryContainer.appendChild(bookContainer);
+
+}
+
+
+
