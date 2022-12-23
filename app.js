e0050e8193505cf63437d53dc07315d23e4cab5a
CaolanCode <cmacmahon3@gmail.com>
2022-12-23 11:48:33 +0000

    add basic js functionality

--- app.js
+++ app.js
@@ -10,3 +10,7 @@ function Book(title, author, pages, read){
 function addBookToLibrary(book){
   myLibrary.push(book);
 }
+
+const book1 = new Book("Atomic Habits", "James Clear", 320, "yes");
+addBookToLibrary(book1);
+console.log(myLibrary[0]);
