7db11e35be21c8d70391f48d6688644d1c47e47f
CaolanCode <cmacmahon3@gmail.com>
2022-12-28 15:30:59 +0000

    commit book is read button

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
-function Library(library = []){
-  this.library = library;
+function Library(books = []){
+  this.books = books;
 }
 Library.prototype.add = function addBookToLibrary(book){
-  this.library.push(book);
-  console.log(this.library);
+  this.books.push(book);
 }
 Library.prototype.remove = function removeBookFromLibrary(index){
-  this.library.splice(index, 1);
+  this.books.splice(index, 1);
 }
 Library.prototype.displayLibrary = function displayLibrary(){
-  createDisplay(this.library);
+  createDisplay(this.books);
 }
 function Book(title, author, pages, read){
   this.title = title;
@@ -24,42 +23,60 @@ function getBookData(){
   let read = false;
   if(document.getElementById('read-input').checked){
     read = true;
-  } else {
-    read = false;
   }
   const book = new Book(titleInput, authorInput, pagesInput, read);
   library.add(book);  
   library.displayLibrary();
   exitForm();
 }
-function createDisplay(library){
+
+const library = new Library();
+const libraryContainer = document.getElementById('library-container');
+
+function createDisplay(books){
   libraryContainer.innerHTML = "";
-  for(let item in library){
+  for(let book in books){
     const bookContainer = document.createElement('div');
     bookContainer.classList.add('book-container')
+    bookContainer.dataset.index = book;
     const titleBox = document.createElement('p');
     const authorBox = document.createElement('p');
     const pagesBox = document.createElement('p');
-    const readBox = document.createElement('p');
-    titleBox.textContent = `Title: ${library[item].title}`;
-    authorBox.textContent = `Author: ${library[item].author}`;
-    pagesBox.textContent = `Pages: ${library[item].pages}`;
-    readBox.textContent = `Have I read this book?: ${library[item].read}`;
+    titleBox.textContent = `Title: ${books[book].title}`;
+    authorBox.textContent = `Author: ${books[book].author}`;
+    pagesBox.textContent = `Pages: ${books[book].pages}`;
     const removeButton = document.createElement('button');
     removeButton.classList.add('remove-button');
     removeButton.innerHTML = "Remove"
-    removeButton.dataset.index = item;
     removeButton.setAttribute('type', 'button');
     removeButton.addEventListener('click', removeBook);
     const readButton = document.createElement('button');
     readButton.setAttribute('type', 'button');
     readButton.classList.add('read-button');
+    readButton.addEventListener('click', changeBookStatus);
+    if(books[book].read === true){
+      readButton.textContent = "Read";
+      readButton.classList.add('book-is-read');
+    } else{
+      readButton.textContent = "Not Read";
+    }
     bookContainer.appendChild(titleBox);
     bookContainer.appendChild(authorBox);
     bookContainer.appendChild(pagesBox);
-    bookContainer.appendChild(readBox);
     bookContainer.appendChild(removeButton);
+    bookContainer.appendChild(readButton);
     libraryContainer.appendChild(bookContainer);
+  }
+}
+function changeBookStatus(event){
+  const index = event.target.parentElement.getAttribute('data-index');
+  event.target.classList.toggle('book-is-read');
+  if(library.books[index].read === true){
+    library.books[index].read = false;
+    event.target.textContent = "Not read";
+  } else{
+    library.books[index].read = true;
+    event.target.textContent = "Read";
   }
 }
 function displayForm(){
@@ -73,13 +90,11 @@ function exitForm(){
   document.getElementById('read-input').checked = false;
 }
 function removeBook(event){
-  const index = event.target.getAttribute('data-index');
+  const index = event.target.parentElement.getAttribute('data-index');
   library.remove(index);
   library.displayLibrary();
 }
 
-const library = new Library();
-const libraryContainer = document.getElementById('library-container');
 
 // form
 const form = document.getElementById('form');
